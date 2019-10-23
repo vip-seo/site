1f7a050cc1e6f336d9714ac3acecdd2ef1e99ef9
vip-seo <csat.vip@gmail.com>
2019-10-23 22:06:37 +0300

    some code #2

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var courses = require('./data/courses.json');
 var app = express();
 
 app.set('view engine', 'pug');
@@ -8,3 +7,19 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+
+app.get('/', function (req, res) {
+    res.render('index', {title: 'Главня страница'});
+  });
+
+app.get('/about', function (req, res) {
+    res.render('about', {title: 'О нас'});
+  });
+
+ app.get('/contact', function (req, res) {
+    res.render('contact', {title: 'Наши контакты'});
+  });
+
+ app.listen(3000, function(){
+    console.log('app at http://localhost:3000');
+  });
