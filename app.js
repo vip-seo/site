937a75586eaa7b37f153c7b9beb288af335573e6
vip-seo <csat.vip@gmail.com>
2019-10-24 00:15:25 +0300

    Some code #7

--- app.js
+++ app.js
@@ -4,22 +4,61 @@ var app = express();
 
 app.set('view engine', 'pug');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Главня страница'});
-  });
+    res.render('index', {
+        title: 'Главня страница'
+    });
+});
 
 app.get('/about', function (req, res) {
-    res.render('about', {title: 'О нас'});
-  });
+    res.render('about', {
+        title: 'О нас'
+    });
+});
 
- app.get('/contact', function (req, res) {
-    res.render('contact', {title: 'Наши контакты'});
-  });
+app.get('/contact', function (req, res) {
+    res.render('contact', {
+        title: 'Наши контакты'
+    });
+});
 
- app.listen(3000, function(){
+app.post('/send', function (req, res) {
+
+    var nodemailer = require('nodemailer');
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'imtjs44@gmail.com',
+            pass: 'l;fdfcrhbgn78'
+        }
+    });
+
+    var mailOptions = {
+        from: req.body.email,
+        to: 'csat.vip@gmail.com',
+        subject: 'Письмо с сайта от ' + req.body.name,
+        text: req.body.message
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.render('send', {
+                title: 'Письмо отправлено'
+            });
+        }
+    });
+
+});
+
+app.listen(3000, function () {
     console.log('app at http://localhost:3000');
-  });
+});
